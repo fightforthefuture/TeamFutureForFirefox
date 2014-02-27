86230a04c35734a4304d5a88d8d4f3e3a5f53c6e
Chris Antaki <ChrisAntaki@gmail.com>
2014-02-27 06:28:48 -0800

    Makeover

--- data/signal.js
+++ data/signal.js
@@ -6,7 +6,9 @@ if ($notification) {
 
 // Update template values.
 var html = self.options.html;
+html = html.replace('$background', self.options.background);
 html = html.replace('$image', self.options.image);
+html = html.replace('$link', self.options.campaign.url);
 html = html.replace('$title', self.options.campaign.name);
 
 // Create notification.
@@ -14,10 +16,18 @@ var $notification = $('<div>');
 $notification.html(html);
 $('body').append($notification);
 
-// Animate.
+// Animate in.
 $notification.children().animate({
     top: '0px'
 });
+
+// Animate spotlight.
+var spotlightAnimation = setInterval(animateSpotlight, 3210);
+function animateSpotlight() {
+    $('#team-future-signal').toggleClass('tilted');
+}
+animateSpotlight();
+
 
 // Event listeners.
 $('#team-future-signal').on('click', function(e) {
@@ -26,6 +36,8 @@ $('#team-future-signal').on('click', function(e) {
     destroy(300);
     
     self.port.emit('clicked');
+
+    e.preventDefault();
 });
 
 $('#team-future-signal #x').on('click', function(e) {
@@ -40,8 +52,10 @@ self.port.on('destroy', destroy);
 
 function destroy(duration) {
     $notification.children().animate({
-        top: '-32px'
+        top: '-200px'
     }, duration || 0, function() {
         $notification.remove();
     });
+
+    clearInterval(spotlightAnimation);
 }
